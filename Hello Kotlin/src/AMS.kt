014aabe224bfd497b5a49cd612f06b46a5e7aba1
Knut Valen <knut.valen@rainfall.no>
2018-05-20 11:47:24 +0200

    • lesson 3.7

--- Hello Kotlin/src/AMS.kt
+++ Hello Kotlin/src/AMS.kt
@@ -2,9 +2,37 @@ import java.util.*
 
 fun main(args: Array<String>) {
     println("Hello ${args[0]}!")
+    println("Good ${ if (args[1].toInt() < 12) "morning" else "night"}, ${args[0]}")
     val temperature = 10
     println("You are ${ if(temperature > 50) "fried" else "safe" } fish")
     dayOfWeek()
+    feedTheFish()
+    for (i in 1..10) {
+        val fortune = getFortuneCookie()
+        println("Your fortune is: $fortune")
+        if (fortune == "Take it easy and enjoy life!") break
+    }
+}
+
+fun getFortuneCookie(): String {
+    val fortunes = listOf("You will have a great day!", "Things will go well for you today.",
+            "Enjoy a wonderful day of success.", "Be humble and all will turn out well.",
+            "Today is a good day for exercising restraint.", "Take it easy and enjoy life!",
+            "Treasure your friends because they are your greatest fortune.")
+    println("Enter your birthday:")
+    val birthday: Int = readLine()?.toIntOrNull() ?: 1
+    return fortunes[birthday.rem(fortunes.size)]
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = "pellets"
+    println("Today is $day and the fish eat $food")
+}
+
+fun randomDay() : String {
+    val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(7)]
 }
 
 fun dayOfWeek() {

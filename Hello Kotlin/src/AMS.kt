f49ca261180c6e4a6f6be91752ece6b14f3e5070
Knut Valen <knut.valen@gmail.com>
2018-09-06 11:03:48 +0200

    • update gitignore • add more functions to the hello kotlin class

--- Hello Kotlin/src/AMS.kt
+++ Hello Kotlin/src/AMS.kt
@@ -7,27 +7,52 @@ fun main(args: Array<String>) {
     println("You are ${ if(temperature > 50) "fried" else "safe" } fish")
     dayOfWeek()
     feedTheFish()
+    val canAddMoreFish = fitMoreFish(10.0, listOf(), 7, true)
+    println("Can add more fish: $canAddMoreFish")
+
     for (i in 1..10) {
-        val fortune = getFortuneCookie()
+        val fortune = getFortuneCookie(getBirthday())
         println("Your fortune is: $fortune")
         if (fortune == "Take it easy and enjoy life!") break
     }
 }
 
-fun getFortuneCookie(): String {
-    val fortunes = listOf("You will have a great day!", "Things will go well for you today.",
-            "Enjoy a wonderful day of success.", "Be humble and all will turn out well.",
-            "Today is a good day for exercising restraint.", "Take it easy and enjoy life!",
+fun fitMoreFish(tankSize: Double, currentFish: List<Int>, fishSize: Int = 2, hasDecorations: Boolean = true): Boolean {
+    val length = currentFish.sum() + fishSize
+    val limit = if (hasDecorations) tankSize.times(0.8) else tankSize
+    return length <= limit
+}
+
+fun getFortuneCookie(birthday: Int): String {
+    val fortunes = listOf("You will have a great day!",
+            "Things will go well for you today.",
+            "Enjoy a wonderful day of success.",
+            "Be humble and all will turn out well.",
+            "Today is a good day for exercising restraint.",
+            "Take it easy and enjoy life!",
             "Treasure your friends because they are your greatest fortune.")
-    println("Enter your birthday:")
-    val birthday: Int = readLine()?.toIntOrNull() ?: 1
-    return fortunes[birthday.rem(fortunes.size)]
+    return when (birthday) {
+        28, 31 -> "this is end of month"
+        in 1..7 -> "this is first week"
+        else -> fortunes[birthday.rem(fortunes.size)]
+    }
 }
 
 fun feedTheFish() {
     val day = randomDay()
-    val food = "pellets"
+    val food = fishFood(day)
     println("Today is $day and the fish eat $food")
+}
+
+fun fishFood(day: String) : String {
+    return when (day) {
+        "Monday" -> "flakes"
+        "Tuesday" -> "redworms"
+        "Wednesday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Sunday" -> "plankton"
+        else -> "fasting"
+    }
 }
 
 fun randomDay() : String {
@@ -48,4 +73,9 @@ fun dayOfWeek() {
         Calendar.SUNDAY -> println("Sunday")
         else -> println("( ͡° ͜ʖ ͡°)")
     }
+}
+
+fun getBirthday() : Int {
+    println("Enter your birthday:")
+    return readLine()?.toIntOrNull() ?: 1
 }
